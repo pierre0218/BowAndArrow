aa992e01c5cd7ec52f26607ff407aee6af360291
peterlee <pierre0218@gmail.com>
2016-06-06 22:45:40 +0800

    上傳project

---
+++ Assets/Scripts/ArcherController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArcherController : MonoBehaviour {
+	Animator mAnimator;
+	bool isPlayingShoot = false;
+	// Use this for initialization
+	void Start () {
+		mAnimator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			ShootArrow();
+		}
+	
+	}
+
+	public void ShootArrow()
+	{
+		mAnimator.SetTrigger ("shootTrigger");
+
+	}
+
+	public void CastArrow()
+	{
+		GameObject arrowGo = Instantiate (Resources.Load("arrow")) as GameObject;
+
+		Vector3 pos = transform.position;
+		pos.x += 0f;
+		pos.y += 0.12f;
+		arrowGo.transform.position = pos;
+	}
+}
---
+++ Assets/Scripts/ArrowController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArrowController : MonoBehaviour {
+	float speed = 20;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 pos = transform.position;
+
+		pos += Vector3.right * Time.deltaTime * speed;
+
+		transform.position = pos;
+
+		if (transform.position.x > 10) {
+			Destroy(gameObject);
+		}
+	}
+}
